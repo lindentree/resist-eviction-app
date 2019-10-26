9646afe59e4ee56bf6af4ae4ad1399f6bb46fd2c
Linden Chiu <hchiu87@gmail.com>
2019-10-26 15:01:01 -0700

    basic route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
+import stop from './stop.jpg';
 import './App.css';
-import LandingPage from './LandingPage';
+//import LandingPage from './LandingPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+const divStyle = {
+  fontSize: '40px',
+  textAlign: 'center'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      
+      <Router>
+      <nav>
+       <div>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/yes">Yes</Link>
+        </li>
+        <Switch>
+          <Route path="/yes">
+            <Yes />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </div>
+      </nav>
+      </Router>
+    
+  );
+  }
+}
+
+function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Do you think you are being evicted?
-        </p>
-      </header>
+        <header className="App-header">
+          <img src={stop} className="App-logo" alt="logo" />
+          <p>
+            Do you think you are being evicted?
+          </p>
+          <Link to="/yes">Yes</Link>
+          
+        </header>
     </div>
-  );
+
+    )
 }
+
+function Yes() {
+  return (
+  <nav style={divStyle}>
+    <h1>
+        Don't Panic 
+    </h1>
+  </nav>
+  )
+}
+
+
+
 
 export default App;
