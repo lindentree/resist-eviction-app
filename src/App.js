a1b2d0421afaa61e38c7ab04797133cf11336996
Jaahanavee Sikri <jaahanaveesikri@gmail.com>
2019-11-08 16:52:07 -0800

    Made changes to UI. Added PDF screenshots.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React from "react";
 //import stop from './stop.jpg';
-import './App.css';
-import LandingPage from './LandingPage';
-import Emergency from './Emergency';
+import "./App.css";
+import LandingPage from "./LandingPage";
+import Emergency from "./Emergency";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // const divStyle = {
 //   fontSize: '40px',
@@ -25,47 +20,38 @@ class App extends React.Component {
 
   render() {
     return (
-      
       <Router>
-      <nav>
-       <div>
-        <Switch>
-          <Route path="/yes" component={LandingPage}>
-            
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
+        <nav>
+          <div>
+            <Switch>
+              <Route path="/yes" component={LandingPage}></Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
 
-        <Route path="/answer" component={Emergency}>
-        </Route>
-        </Switch>
-        </div>
-      </nav>
+              <Route path="/answer" component={Emergency}></Route>
+            </Switch>
+          </div>
+        </nav>
       </Router>
-    
-  );
+    );
   }
 }
 
 function Home() {
   return (
     <div className="App">
-        <header className="App-header">
-          <h1>Resist Eviction in California</h1>
-          <p>
-            Are you being evicted? You can fight it.
-          </p>
-          <Link to="/yes">Learn More</Link>
-          
-        </header>
+      <header className="App-header">
+        <h1>Resist Eviction in California</h1>
+        <p>Are you being evicted? You can fight it.</p>
+        <button type="button" className="btn btn-info">
+          <Link className="link" to="/yes">
+            Learn More
+          </Link>
+        </button>
+      </header>
     </div>
-
-    )
+  );
 }
-
-
-
-
 
 export default App;
